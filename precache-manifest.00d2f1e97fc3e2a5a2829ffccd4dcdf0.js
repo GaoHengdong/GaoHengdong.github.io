d45d97607f8c6248d5a368536281e62bc7b55a7b
GaoHengdong <hengdonggao@outlook.com>
2019-05-21 00:18:48 +0800

    Updates

---
+++ precache-manifest.00d2f1e97fc3e2a5a2829ffccd4dcdf0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8c8a567cc324487a2c4765b52df9a",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "0be58fe4cd86faabb63b",
+    "url": "/blog/static/css/2.1c7ffa40.chunk.css"
+  },
+  {
+    "revision": "b56236eaa558dab2e840",
+    "url": "/blog/static/css/main.821af09a.chunk.css"
+  },
+  {
+    "revision": "0be58fe4cd86faabb63b",
+    "url": "/blog/static/js/2.0e13680f.chunk.js"
+  },
+  {
+    "revision": "b56236eaa558dab2e840",
+    "url": "/blog/static/js/main.8dff48d5.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "383bb8976c03ae8cf13f5602c98ac72e",
+    "url": "/blog/static/media/SVG的总结.383bb897.md"
+  },
+  {
+    "revision": "2704acfa726a0212f0204706aff081b8",
+    "url": "/blog/static/media/js中rem和px转换的方法.2704acfa.md"
+  },
+  {
+    "revision": "9e5b075b60126d262c3b1b9a6be1d7ad",
+    "url": "/blog/static/media/mutates vs no mutation.9e5b075b.md"
+  },
+  {
+    "revision": "1f16c3fdc9c6784a63b4b6b2eb544f03",
+    "url": "/blog/static/media/ruby添加到gitbash.1f16c3fd.md"
+  },
+  {
+    "revision": "a864ec110db0158b2a3a2cfe91b0af2c",
+    "url": "/blog/static/media/z-index的各种问题.a864ec11.md"
+  },
+  {
+    "revision": "933c35d4bbc6b652c790f0d696152777",
+    "url": "/blog/static/media/各种遍历的区别.933c35d4.md"
+  },
+  {
+    "revision": "d00ac814a95417a135f7ade41ead7e1c",
+    "url": "/blog/static/media/正则表达式.d00ac814.md"
+  },
+  {
+    "revision": "28ca3634ae52f34322eaa21af3eb015c",
+    "url": "/blog/static/media/特别鸣谢.28ca3634.md"
+  },
+  {
+    "revision": "728c4c8c97c1f80ed3c3939fa02842ea",
+    "url": "/blog/static/media/论成功和失败的关系.728c4c8c.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b36c4b7c78d5c4d443ef9c86ad048e42.js"
+  "/blog/precache-manifest.00d2f1e97fc3e2a5a2829ffccd4dcdf0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
